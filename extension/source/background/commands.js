912e46f8c06c59942a8cb3c4ba092a7e172dd9bc
Nick McDonald <nick.mcdonald@gmail.com>
2019-12-10 14:16:10 -0500

    Add keyboard shortcut to initiate parsing on a webpage

--- extension/source/background/commands.js
+++ extension/source/background/commands.js
@@ -1,5 +1,22 @@
+function parse_dom() {
+    console.log('Tab script:');
+    console.log(document.body);
+    return document.body.innerHTML;
+}
+
+function get_page_content() {
+    chrome.tabs.executeScript({
+        code: '(' + parse_dom + ')();' //argument here is a string but function.toString() returns function's code
+    }, (results) => {
+        //Here we have just the innerHTML and not DOM structure
+        console.log('Popup script:')
+        console.log(results[0]);
+    });
+}
+
 chrome.commands.onCommand.addListener(function(command) {
     if (command == 'parse-recipe-page') {
+        get_page_content();
         alert('Parse page for recipe');
     }
 });
